51f664e13685e3597d0e1dc470689664475daaf6
hernandez87v <hernandez87v@hotmail.com>
2020-06-03 20:49:36 -0700

    Change to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const Footer = props => (
   <footer id="footer" style={props.timeout ? { display: 'none' } : {}}>
-    <p className="copyright">&copy; Vlad Hernandez </p>
+    <p className="copyright">&copy; 2020 Vlad Hernandez</p>
   </footer>
 )
 
